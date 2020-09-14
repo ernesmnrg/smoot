5eb7906a755416c6653fce0c44c57e0658bf7381
Ernest <ernesmanurungg@gmail.com>
2020-09-14 11:33:57 +0700

    [Ernest] - Add JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".marquee").slick({
+  speed: 9000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: "linear",
+  slideToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  infinite: true,
+});
